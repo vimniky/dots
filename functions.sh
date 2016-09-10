0480ce0f8b08de1471799e041365ab652bf02532
vimniky <vimniky@gmail.com>
2016-09-11 00:30:39 +0800

    add log function

--- functions.sh
+++ functions.sh
@@ -64,3 +64,12 @@ function :h () {
 function gcopy() {
     git rev-parse --short @ | tr -d '\n' | pbcopy && echo "Copied `pbpaste`"
 }
+
+log() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
+      --bind "ctrl-m:execute:
+                echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R'"
+}
