7cd6a5572e1d7cff37f930538f9627be03632cfa
vimniky <vimniky@gmail.com>
2017-03-26 20:36:17 +0800

    Sun Mar 26 20:36:17 CST 2017

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
-# go
-export GOPATH="$HOME/langs/go"
-export PATH="$GOPATH/bin:$PATH"
-
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
+
+# Must place before: [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$PATH"
