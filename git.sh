3c2bf275e8c7d66a86bcbe10cce21cf0cf9ad0cf
vimniky <vimniky@gmail.com>
2017-03-26 20:09:43 +0800

    Sun Mar 26 20:09:43 CST 2017

--- git.sh
+++ git.sh
@@ -15,9 +15,9 @@ function git-commit-emacsrc() {
   git add .
   if [ -z "$1" ]
   then
-    git commit -m "$1"
-  else
     git commit -m "$(date)"
+  else
+    git commit -m "$1"
   fi
   git push
 }
