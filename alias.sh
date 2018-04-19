c837e59a409277a7fac1ed67c79f2b76b4f628ef
vimniky <vimniky@gmail.com>
2018-04-19 22:37:04 +0800

    Update alias

--- alias.sh
+++ alias.sh
@@ -19,6 +19,9 @@ alias treepa="tree --noreport -fpa"
 alias ghc-sandbox="ghc -no-user-package-db -package-db .cabal-sandbox/*-packages.conf.d"
 alias ghci-sandbox="ghci -no-user-package-db -package-db .cabal-sandbox/*-packages.conf.d"
 alias runhaskell-sandbox="runhaskell -no-user-package-db -package-db .cabal-sandbox/*-packages.conf.d"
+alias yd="youtube-dl"
+alias py2=python
+alias py=python3
 # git alias
 alias gmm='git commit -m'
 alias gma='git commit --amend'
